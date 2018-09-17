dfe91a0f6e16cd1a4a336d94f51c7666d7db14ea
vagrant <vagrant@homestead>
2018-09-17 16:01:24 +0000

    admin masterpage setup

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('admin/users','AdminUsersController');
+
+Route::GET('/admin', function(){
+    return view('admin.index');
+});
