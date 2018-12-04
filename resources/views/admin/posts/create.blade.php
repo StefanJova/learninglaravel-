0c456f1bb7164846cdf4a472a18136d7ab36e3d6
vagrant <vagrant@homestead>
2018-12-04 14:42:22 +0000

    posts with dynamic categories

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -9,7 +9,7 @@
         </div>
         <div class="form-group">
             {!! Form::label('category_id', 'Category:') !!}
-            {!! Form::select('category_id',array('0'=>'JavaScript','1'=>'PHP'), null,['class'=>'form-control']) !!}
+            {!! Form::select('category_id',[''=>'Choose category'] + $categories, null,['class'=>'form-control']) !!}
         </div>
         <div class="form-group">
             {!! Form::label('photo_id', 'Photo:') !!}
